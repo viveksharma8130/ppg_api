f79293078b5526863c8e6b4fccb4efca5ea75a3b
Vipul <vipulranakumar>
2022-03-03 12:54:56 +0530

    second

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ let server = new Server().app;
 let port =  process.env.PORT || 5200;
 
 server.listen(port, () => {
-    console.log(`PujyapanditG : PORT ${port}`);
+    console.log(`PujyapanditG Api : PORT ${port}`);
 });
 
 
