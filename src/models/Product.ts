71559985e1d2c741acbc73639ac1a33a2d54038c
vivek <vipulranakumar>
2022-03-10 16:49:16 +0530

    as

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -6,10 +6,12 @@ const ProductSchema = new mongoose.Schema({
     name                     : {type: String, required: true},
     description              : {type: String, required: false},
     short_description        : {type: String, required: false},
-    image                    : {type: String, required: false},
-    image_name               : {type: String, required: false},
-    mrp                      : {type: Number, required: true},
-    price                    : {type: String, required: true},
+    category                 : {type: mongoose.Types.ObjectId, ref: 'categorys', required: false}, // cloth - size & material, length
+    //image                    : {type: String, required: false},
+    //image_name               : {type: String, required: false},
+    //mrp                      : {type: Number, required: true},
+    //price                    : {type: String, required: true},
+    variant                  : {type: String, required: true}, // [{id:'1', mrp:'200', price:'150', size:'M', material:'Cotton', Colour:'Brown'},{id:'2', mrp:'200',price:'150',size:'M',material:'COTTON',COLOUR:'BLACK'}]
     brand                    : {type: String, required: true},
     sequence                 : {type: Number, required: false, default:1},
     ishome                   : {type: Boolean, required: true, default: false},
